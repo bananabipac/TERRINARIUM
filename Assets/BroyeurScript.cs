1f87137e3caad536d31ace2023c85ad5e8a58c72
jordan dos santos <dossatos.jordan94@gmail.com>
2012-06-15 22:03:25 +0200

    commit entonoire

---
+++ Assets/BroyeurScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BroyeurScript : MonoBehaviour {
+	
+	public GameObject lapinStadeFinal;
+	public List<GameObject> transformation;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
